2ab63c11bca9f089f2d2c3681235da0cfd62a130
mariano alvarez <mariano@codingco.io>
2023-03-22 11:59:05 -0300

    /

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./navbar.css";
 
+import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
+
 import logo from "../../assets/dauralogo.png";
 
 const Navbar = () => {
   return (
     <header>
-      <div className="container">
+      <motion.div 
+      initial={{opacity:0}}
+      animate={{opacity: 1}}
+      transition={{ duration: 1 }}
+      exit={{}}
+      className="container">
         <div className="row">
           <div className="col-xl-12">
             <ul className="nav-links">
@@ -22,7 +29,7 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </div>
+      </motion.div>
     </header>
   );
 };
--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./proyectos.css";
 
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
+
 import proyectos from "../../utils/proyectos";
 
 const Proyectos = () => {
@@ -15,7 +17,12 @@ const Proyectos = () => {
       <div className="container">
         <div className="row">
           <div className="col-xl-12">
-            <div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
               <ul className="proyect-list">
                 <li>
                   <a
@@ -54,12 +61,19 @@ const Proyectos = () => {
                   </a>
                 </li>
               </ul>
-            </div>
-            <div className="wrapper">
-              {proyectos.map((proyecto, index) => (
-                <img src={proyecto} alt={`Image ${index}`} key={index} />
-              ))}
-            </div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <div className="wrapper">
+                {proyectos.map((proyecto, index) => (
+                  <img src={proyecto} alt={`Image ${index}`} key={index} />
+                ))}
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
