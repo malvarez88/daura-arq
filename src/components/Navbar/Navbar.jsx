4956b4ef40086c0a29e1400cc9759a23f29b5537
mariano alvarez <mariano@codingco.io>
2023-04-12 11:27:21 -0300

    /

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,70 +9,91 @@ import menu from "../../assets/bars-solid.svg";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-
   const handleToggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  const [isScrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const handleScroll = () => {
+    if (window.pageYOffset > 250) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ ease: "linear", duration: 1 }}
-      exit={{ opacity: 0, y: -100 }}
-    >
-      <div className="container">
-        <div className="row">
-          <div className="col-xl-12">
-            <div className="navbar">
-              <div className="left-container">
-                <a href="/proyectos" className="nav-link">
-                  Proyectos
-                </a>
-              </div>
-              <div className="center-container">
-                <a href="/">
-                  <img src={logo} alt="logo" />
-                </a>
-              </div>
-              <div className="rigth-container">
-                <a href="/estudio" className="nav-link">
-                  Estudio
-                </a>
+    <header className="header">
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: "linear", duration: 1 }}
+        exit={{ opacity: 0, y: -100 }}
+      >
+        
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              <div className={`navbar ${isScrolled && "header-scrolled"}`}>
+                <div className="left-container">
+                  <a href="/proyectos" className="nav-link">
+                    Proyectos
+                  </a>
+                </div>
+                <div className="center-container">
+                  <a href="/">
+                    <img src={logo} alt="logo" />
+                  </a>
+                </div>
+                <div className="rigth-container">
+                  <a href="/estudio" className="nav-link">
+                    Estudio
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-xl-12">
-            <div className="mobile-navbar">
-              <div className="logo">
-                <a href="/">
-                  <img src={logo} alt="logo" width="70px" />
-                </a>
-              </div>
-              <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={handleToggleMenu}>
-                <div className="line"></div>
-                <div className="line"></div>
-                <div className="line"></div>
-              </div>
-              <div className={`menu ${isOpen ? "open" : ""}`}>
-                <ul className="navbar-menu">
-                  <li className="mobile-link">
-                    <a href="/proyectos">Proyectos</a>{" "}
-                  </li>
-                  <li className="mobile-link">
-                    <a href="/espacios">Espacios</a>{" "}
-                  </li>
-                  <li className="mobile-link">
-                    <a href="/estudio">Estudio</a>{" "}
-                  </li>
-                </ul>
+            <div className="col-xl-12">
+              <div className="mobile-navbar">
+                <div className="logo">
+                  <a href="/">
+                    <img src={logo} alt="logo" width="70px" />
+                  </a>
+                </div>
+                <div
+                  className={`hamburger ${isOpen ? "open" : ""}`}
+                  onClick={handleToggleMenu}
+                >
+                  <div className="line"></div>
+                  <div className="line"></div>
+                  <div className="line"></div>
+                </div>
+                <div className={`menu ${isOpen ? "open" : ""}`}>
+                  <ul className="navbar-menu">
+                    <li className="mobile-link">
+                      <a href="/proyectos">Proyectos</a>{" "}
+                    </li>
+                    <li className="mobile-link">
+                      <a href="/espacios">Espacios</a>{" "}
+                    </li>
+                    <li className="mobile-link">
+                      <a href="/estudio">Estudio</a>{" "}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </motion.div>
+      
+      </motion.div>
+     </header>
   );
 };
 
