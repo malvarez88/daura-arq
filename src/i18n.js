f832de2559d38a7468242c7c6fff0bb9eb7efef0
henry H <hlace.henry@gmail.com>
2023-05-24 03:26:04 -0300

    detail

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,7 @@
 import i18next from "i18next";
-
-import store from "./store/store";
-
 import global_es from "./languages/es/global.json";
 import global_en from "./languages/en/global.json";
 import global_ca from "./languages/ca/global.json";
-
-const language = store.getState().language;
-
-
 
 i18next.init({
   interpolation: {
