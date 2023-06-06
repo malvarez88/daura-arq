cac7c21bbbaa1eba5358cb788add2f1981392020
hhlace <hlace.henry@gmail.com>
2023-06-06 19:08:14 -0300

    team order fix (#39)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './i18n';
 import "./index.css";
-// import store from "./store/store";
 
 
 
@@ -13,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider store={store}> */}
-          <App />
-      {/* </Provider> */}
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
