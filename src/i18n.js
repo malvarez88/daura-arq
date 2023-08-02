3feae497f815f76cde6cf279e63e560a1e6bf1be
mariano alvarez <mariano@codingco.io>
2023-08-02 14:51:28 -0300

    nosotros y equipo strapi

--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,27 @@
-import i18next from "i18next";
+import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import global_es from "./languages/es/global.json";
-import global_en from "./languages/en/global.json";
-import global_ca from "./languages/ca/global.json";
+import global_es from './languages/es/global.json';
+import global_en from './languages/en/global.json';
+import global_ca from './languages/ca/global.json';
 
 i18next
-.use(initReactI18next)
-.init({
-  interpolation: {
-    escapeValue: false,
-  },
-  lng: "es", 
-  resources: {
-    es: {
-      global: global_es,
+  .use(initReactI18next)
+  .init({
+    interpolation: {
+      escapeValue: false,
     },
-    en: {
-      global: global_en,
+    lng: 'es',
+    resources: {
+      es: {
+        global: global_es,
+      },
+      en: {
+        global: global_en,
+      },
+      ca: {
+        global: global_ca,
+      },
     },
-    ca: {
-      global: global_ca,
-    },
-  },
-});
+  });
 
 export default i18next;
--- src/pages/Estudio/Estudio.jsx
+++ src/pages/Estudio/Estudio.jsx
@@ -1,35 +1,45 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import s from "./estudio.module.css";
 
-import {
-  about1,
-  about2,
-  about3,
-  wbf1,
-} from "../../assets";
+import { about1, about2, about3, wbf1 } from "../../assets";
 import { equipo } from "../../constants";
 import { changeDocTitle } from "../../hooks/hooks";
+import { axiosInstance } from "../../services/axiosInstance";
 
-const Estudio = () => {
+function Estudio() {
+  const [about, setAbout] = useState(null);
+
   const TABS = {
     US: "Us",
-    TEAM: "Team"
+    TEAM: "Team",
   };
   const [activeTab, setActiveTab] = useState(TABS.US);
 
-  const { t } = useTranslation("global");
+  const { t, i18n } = useTranslation("global");
 
-  const location = t('navbar.estudio')
+  const locale = i18n.language;
+
+  const location = t("navbar.estudio");
 
   useEffect(() => {
-    changeDocTitle(location)
-  }, [location])
+    changeDocTitle(location);
+  }, [location]);
 
   function handleClick(tab) {
     setActiveTab(tab);
   }
+
+  const getAboutUs = async () => {
+    const { data } = await axiosInstance().get(`/nosotro?locale=${locale}`);
+    setAbout(data.attributes);
+  };
+
+  useEffect(() => {
+    getAboutUs();
+  }, [locale]);
 
   return (
     <motion.div
@@ -56,25 +66,39 @@ const Estudio = () => {
               </ul>
               {/* SOBRE NOSOTROS */}
               <div
-
                 style={{ display: activeTab === TABS.US ? "block" : "none" }}
               >
                 <h5 className="title">d'aura arquitectura</h5>
                 <p className="general-text">
-                  {t("estudio-page.sobre-nosotros-1")}
+                  {/* {t('estudio-page.sobre-nosotros-1')} */}
+                  {about?.nosotros1}
                 </p>
 
                 <div className={s.sobreNosotrosImg}>
-                  <img src={about1} alt="about" className={`img-fluid img-team ${s.mobileFullWidth}`} />
-                  <img src={about2} alt="about" className={`img-fluid img-team ${s.mobileFullWidth}`} />
-                  <img src={about3} alt="about" className={`img-fluid img-team ${s.mobileFullWidth}`} />
+                  <img
+                    src={about1}
+                    alt="about"
+                    className={`img-fluid img-team ${s.mobileFullWidth}`}
+                  />
+                  <img
+                    src={about2}
+                    alt="about"
+                    className={`img-fluid img-team ${s.mobileFullWidth}`}
+                  />
+                  <img
+                    src={about3}
+                    alt="about"
+                    className={`img-fluid img-team ${s.mobileFullWidth}`}
+                  />
                 </div>
 
-                <p className="general-text">
-                  {t("estudio-page.sobre-nosotros-2")}
-                </p>
+                <p className="general-text">{about?.nosotros2}</p>
                 <div>
-                  <img src={wbf1} alt="" className={`img-fluid img-team ${s.mobileFullWidth}`} />
+                  <img
+                    src={wbf1}
+                    alt=""
+                    className={`img-fluid img-team ${s.mobileFullWidth}`}
+                  />
                 </div>
               </div>
 
@@ -83,11 +107,15 @@ const Estudio = () => {
                 className="eqiupo"
                 style={{ display: activeTab === TABS.TEAM ? "block" : "none" }}
               >
-                <p className="general-text">{t("estudio-page.equipo")}</p>
+                <p className="general-text">{about?.equipo}</p>
                 <div className={s.equipoProfile}>
-                  {equipo.map((member, index) => (
-                    <div className={s.profile} key={index}>
-                      <img src={member.image} alt={member.name} className={`img-fluid img-team ${s.mobileFullWidth}`} />
+                  {equipo.map((member) => (
+                    <div className={s.profile} key={member}>
+                      <img
+                        src={member.image}
+                        alt={member.name}
+                        className={`img-fluid img-team ${s.mobileFullWidth}`}
+                      />
                       <p>{member.name}</p>
                     </div>
                   ))}
@@ -99,6 +127,6 @@ const Estudio = () => {
       </div>
     </motion.div>
   );
-};
+}
 
 export default Estudio;
